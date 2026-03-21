fc2a0b0c3ce69039936d1641caa11c2a24ff2a39
dfs5974 <dfs5974@g.rit.edu>
2026-03-20 21:49:24 -0400

    shop

--- My project/Assets/Scripts/Managers/GameController.cs
+++ My project/Assets/Scripts/Managers/GameController.cs
@@ -2,11 +2,27 @@ using DangryGames;
 using UnityEngine;
 
 
-class GameController : MonoBehaviour
+public class GameController : MonoBehaviour
 {
-    
+    public int level;
+    public int health;
+    public int bulletDamage;
+    public int meleeDamage;
+    public int bulletCount;
+    public int coinCount;
+    public float speedFactor;
+
+    public int healthTier = 0;
+    public int bulletTier = 0;
+    public int meleeTier = 0;
+    public int bulletCountTier = 0;
+    public int speedTier = 0;
+
+
     public void Awake()
     {
         
     }
+
+
 }
